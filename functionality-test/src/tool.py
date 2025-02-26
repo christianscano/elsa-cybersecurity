f6578014c80f542d3b79a5c28fa1dbda35ab229e
Diesoi <73546287+Diesoi@users.noreply.github.com>
2025-02-26 17:09:00 +0100

    Update tool.py

--- functionality-test/src/tool.py
+++ functionality-test/src/tool.py
@@ -1,5 +1,5 @@
 from typing import List
-import subprocess
+import subprocess, os
 import shutil
 
 class Adb:
@@ -55,6 +55,84 @@ class Adb:
             return pid
         except:
             return None
+    
+    def push_file(self, device: str, file_path: str, directory: str) -> None:
+
+        """
+
+        push a file inside a device
+
+        Parameters
+        ----------
+        device : str
+            device id where to uninstall the application
+        file_path : str
+            file_path to push
+        directory:
+            directory where to push the file
+        """
+
+        cmd = List[str] = [
+            self.adb_path,
+            "-s",
+            device,
+            "push",
+            file_path,
+            directory
+        ]
+        # cmd = " ".join(cmd)
+        # os.system(cmd)
+        subprocess.call(cmd, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+    
+    def chmod(self, device: str, file_path: str, permissions: str):
+        """
+        chmod for a file in a device
+
+        Parameters
+        ----------
+        device : str
+            device id where to uninstall the application
+        file_path : str
+            file_path to push
+        permissions : str
+            permissions to give to the file
+        """
+
+        cmd = List[str] = [
+            self.adb_path,
+            "-s",
+            device,
+            "shell",
+            "chmod",
+            permissions,
+            file_path
+        ]
+        # cmd = " ".join(cmd)
+        # os.system(cmd)
+        subprocess.call(cmd, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+    
+    def execute_file(self, device: str, file_path: str):
+        """
+        chmod for a file in a device
+
+        Parameters
+        ----------
+        device : str
+            device id where to uninstall the application
+        file_path : str
+            file_path to push
+        """
+
+        cmd = List[str] = [
+            self.adb_path,
+            "-s",
+            device,
+            "su 0 sh -c",
+            f"'{file_path} &'"
+        ]
+        # cmd = " ".join(cmd)
+        # os.system(cmd)
+        subprocess.call(cmd, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
 
         
     def uninstall_pkg(self, device: str, pkg_name: str):
