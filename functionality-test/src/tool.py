ca8d022db35b282c0242980407572548f3ec1dc1
Diesoi <73546287+Diesoi@users.noreply.github.com>
2025-03-03 16:33:17 +0100

    updated
    Added something to bypass installation popup

--- functionality-test/src/tool.py
+++ functionality-test/src/tool.py
@@ -94,6 +94,33 @@ class Adb:
         # os.system(cmd)
         subprocess.call(cmd, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
     
+    def pull_file(self, file_path: str, directory: str) -> None:
+
+        """
+
+        pull a file from a device
+
+        Parameters
+        ----------
+        file_path : str
+            file_path to pull
+        directory:
+            directory where to pull the file
+        """
+
+        cmd: List[str] = [
+            self.adb_path,
+            "-s",
+            self.device,
+            "pull",
+            file_path,
+            directory
+        ]
+        # cmd = " ".join(cmd)
+        # os.system(cmd)
+        subprocess.call(cmd, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+
+
     def chmod(self, file_path: str, permissions: str) -> None:
         """
         chmod for a file in a device
@@ -145,6 +172,101 @@ class Adb:
         # os.system(cmd)
         # print(cmd)
         subprocess.Popen(cmd, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+
+    def _get_ui_xml(self, file_path: str, destination: str) -> None:
+        """
+        get the UI xml
+
+        Parameters
+        ----------
+        file_path : str
+            file path where to save remotely (locally to device) the UI
+        destination : str
+            destination where to save on the pc the UI
+        """
+
+        # dump the xml
+        cmd: List[str] = [
+            self.adb_path,
+            "-s",
+            self.device,
+            "shell",
+            "uiautomator",
+            "dump",
+            file_path
+        ]
+        subprocess.run(cmd, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+        self.pull_file(file_path, destination)
+        return destination
+    
+    @staticmethod
+    def _check_install_popup(file_path: str) -> bool:
+        """
+        check the presence of the installation popup
+
+        Parameters
+        ----------
+        file_path : str
+            file path of the UI to read
+
+        Returns
+        -------
+        bool
+            True if the popus USB install is present, False otherwise
+        """
+
+        with open(file_path, "r", encoding="utf-8") as file:
+            return "Installa tramite USB" in file.read()
+    
+    def touch_screen(self, x: str, y: str) -> None:
+        """
+        touch the screen at coordinates (x, y)
+
+        Parameters
+        ----------
+        x : int
+            x coordinate of the screen
+        y : int
+            y coordinate of the screen
+        """
+        
+        cmd: List[str] = [
+            self.adb_path,
+            "-s",
+            self.device,
+            "shell",
+            "input",
+            "tap",
+            x,
+            y
+        ]
+
+        subprocess.run(cmd, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+
+    
+    def install_apk(self, path_app: str) -> None:
+        """
+        Install app given its path
+
+        Parameters
+        ----------
+        path_app : str
+            path of the application to install
+        """
+
+        cmd: List[str] = [
+            self.adb_path,
+            "-s",
+            self.device,
+            "install",
+            path_app
+        ]
+
+        subprocess.Popen(cmd, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+        self._get_ui_xml('/sdcard/ui_dump.xml', ".")
+        # if the installation popup is present touch Install
+        if self._check_install_popup('./ui_dump.xml'):
+            self.touch_screen("293", "2062")
 
         
     def uninstall_pkg(self, pkg_name: str) -> None:
